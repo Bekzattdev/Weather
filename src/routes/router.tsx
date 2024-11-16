05812731023e3769c36de72359aebcd82f175b07
Bekzattdev <alpberlinfox@gmail.com>
2024-11-16 18:26:29 +0600

    update

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,17 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../components/layout/Layout";
+// import Layout from "../components/layout/Layout";
 import HomePage from "../components/pages/HomePage";
+// import HomePage from "../components/pages/HomePage";
 
 export const router: ReturnType<typeof createBrowserRouter> =
   createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/",
-          element: <HomePage />,
-        },
-      ],
+      element: <HomePage />,
     },
   ]);
